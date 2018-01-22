ff740440fb7b08c1dd7dcd7de5dd018103427099
oleh1 <lyboleg@gmail.com>
2018-01-22 14:43:47 +0200

    dsfg

--- asd.php
+++ asd.php
@@ -1,5 +1,6 @@
 <?php
 $n = $_POST['n'];
+$n1 = $_POST['n'];
 $ma = $_POST['ma'];
 $t = $_POST['t'];
 $m = $_POST['m'];
@@ -11,7 +12,7 @@ if(!$n){
 $subject = $n;
 $message = 'Тема - '.$t."\r\n";
 $message .= 'Почта - '.$ma."\r\n";
-$message .= 'Имя - '.$n."\r\n";
+$message .= 'Имя - '.$n1."\r\n";
 $message .= 'Сообщение - '.$m;
 
 if(!$ma){
