c9d1365980873c0b5308e20c32250d66def6d99a
oleh1 <lyboleg@gmail.com>
2018-01-22 12:11:59 +0200

    10

--- asd.php
+++ asd.php
@@ -5,8 +5,9 @@ $t = $_POST['t'];
 $m = $_POST['m'];
 
 $to      = 'lyboleg@gmail.com';
-$subject = $t;
-$message = $m;
+$subject = $n;
+$message = 'Тема - '.$t."\r\n";
+$message .= 'Сообщение - '.$m;
 $headers = 'From: <'.$ma.'>';
 
 mail($to, $subject, $message, $headers);
